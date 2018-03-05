c8f055f06e380d279c2f1c2643281f7367fcb099
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2018-03-04 19:43:09 -0700

    Assignment 2 Task 1

--- assignment_2_task_1.m
+++ assignment_2_task_1.m
@@ -1,16 +1,21 @@
-filenames = dir('DM07/About*.csv');
-for i=1:20
-    x = filenames(i);
-    name = x.name;
-    about = readtable(strcat('DM07/',name));
-    x1 = about(:,1:67);
-    A = table2array(x1);
-    B_temp = A.';
-    if i==1
-        B = B_temp; 
-    else
-        B = cat(1, B, B_temp);
+words = ["About","And","Can","Cat","Cop","Cost","Day","Deaf","Decide","Father","Find","Gold","Goodnight","GoOut","Hearing","Here","Hospital","If"];
+folderName = "DM07/";
+for i=1:length(words)
+    fileNameRegex = strcat(folderName,words(i),'*.csv');
+    fileNames = dir(char(fileNameRegex));
+    for j=1:length(fileNames)
+        fileObj = fileNames(j);
+        fileName = fileObj.name;
+        fileContent = readtable(strcat(folderName,fileName),'ReadVariableNames', false);
+        table = fileContent(1:end,1:end-5);
+        content = table2array(table);
+        contentTransposed = content.';
+        if j==1
+            concatenatedContent = contentTransposed;
+        else
+            concatenatedContent = cat(1, concatenatedContent, contentTransposed);
+        end
     end
-    %B = [B , B_temp];
-    %B = cat(1, B, B_temp); 
+    csvwrite(strcat(words(i),'.csv'),concatenatedContent);
+    concatenatedContent = contentTransposed;
 end
