6e4287ab57a04ce4a7de22dc97a63c7f6aacfcd3
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2018-04-01 02:03:20 -0700

    Assignment - 2 end, start of Assignment 3

--- assignment_2_task_1.m
+++ assignment_2_task_1.m
@@ -1,20 +1,25 @@
+% Task 1: Program to segement raw data into individual classes
+% 
+% 10 csv files, 1 for each gesture will be created under the folder
+% Task-1-Out
 numOfFeatures = 34;
 maxTimeLength = 45;
-wordsForMatch = ["About","And","Can","Cop","Deaf","Decide","Father","Find","Out","Hearing"];
 words = ["About","And","Can","Cop","Deaf","Decide","Father","Find","GoOut","Hearing"];
 folderName = "DM";
 outputFolder = 'Task-1-Output';
 if ~exist(outputFolder, 'dir')
     mkdir(outputFolder);
 end
-for i=1:length(wordsForMatch)
+% Looping for all gestures
+for i=1:length(words)
     fileNameRegex = strcat(folderName,'/',folderName,'*/','*.csv');
     fileNames = dir(char(fileNameRegex));
     concatenatedContent = [];
+    % Looping for all files per gestures
     for j=1:length(fileNames)
         fileObj = fileNames(j);
         fileName = fileObj.name;
-        regEx = strcat('\w*',wordsForMatch(i),'\w*','.csv');
+        regEx = strcat('\w*',words(i),'\w*','.csv');
         startIndex = regexpi(fileName,regEx);
         if isempty(startIndex)
             continue
@@ -25,17 +30,24 @@ for i=1:length(wordsForMatch)
         contentTransposed = content.';
         [x1,y1] = size(contentTransposed);
         if y1>maxTimeLength
+            % Neglecting the action which was recorded for more than 45
+            % seconds
             continue
         elseif y1<maxTimeLength
+            % Padding zeros for actions which have data for less than 45
+            % seconds
             padding = zeros(x1,maxTimeLength-y1);
             contentTransposed = cat(2,contentTransposed,padding);
         end
+        % Concatenating data of different actions of the same gesture
         if isempty(concatenatedContent)
             concatenatedContent = contentTransposed;
         else
             concatenatedContent = cat(1, concatenatedContent, contentTransposed);
         end
     end
+    % Writing the data of each gesture into the csv files which is fed for
+    % task 2
     csvwrite(strcat(outputFolder,'/',words(i),'.csv'),concatenatedContent);
     concatenatedContent = contentTransposed;
 end
--- assignment_2_task_2_final.m
+++ assignment_2_task_2_final.m
@@ -1,18 +1,22 @@
-statisticalFeatures = ["FFT","DWT","RMS","STD"];
-sensorsForStats = ["EMG0R","EMG7R","EMG1R","EMG0L","EMG5R";
-    "OYR","EMG4L","EMG3L","EMG7R","EMG2R";
-    "EMG6L","EMG4L","EMG0L","EMG5L","EMG7L";
-    "EMG6L","EMG4L","ORL","ALY","EMG5L"];
+% Task 2: Program to extract features and obtain feature matrix for each
+% gesture
+statisticalFeatures = ["FFT","DWT","RMS","STD","AVG"];
+sensorsForStats = ["EMG3R","EMG5R","EMG6R","EMG4R","EMG2R";
+    "EMG4R","EMG7R","EMG3R","EMG0R","GLZ";
+    "EMG6L","EMG2L","EMG7L","EMG0L","EMG3L";
+    "OPL","EMG2L","EMG6L","ALX","OYL";
+    "GRY","GRX","GRZ","ARX","GLY"];
 sensorNames = ["ALX","ALY","ALZ","ARX","ARY","ARZ","EMG0L","EMG1L","EMG2L","EMG3L","EMG4L","EMG5L","EMG6L","EMG7L","EMG0R","EMG1R","EMG2R","EMG3R","EMG4R","EMG5R","EMG6R","EMG7R","GLX","GLY","GLZ","GRX","GRY","GRZ","ORL","OPL","OYL","ORR","OPR","OYR"];
 numOfFeatures = 34;
 words = ["About","And","Can","Cop","Deaf","Decide","Father","Find","GoOut","Hearing"];
 maxTimeLength = 45;
 inputFolder = 'Task-1-Output';
 outputFolderName = strcat('Task-2-Output-Final');
-
+numberOfPeakValues = 4;
 if ~exist(outputFolderName, 'dir')
     mkdir(char(outputFolderName));
 end
+% Looping over gestures
 for i=1:length(words)
     fileName = strcat(inputFolder,'/',words(i),'.csv');
     file = readtable(fileName,'ReadVariableNames',false);
@@ -21,32 +25,39 @@ for i=1:length(words)
     numOfActions = x / numOfFeatures;
     concatenatedContentAfterAllAlgo = [];
     xAxisNames = [];
+    % Looping over different techniques
     for j=1:length(statisticalFeatures)
+        % Looping over different sensors for each technique
         for sensorIndex=1:length(sensorsForStats(j,1:end))
             concatenatedContentAfterAlgo = [];
             sensorName = sensorsForStats(j,sensorIndex);
             featureIndex = find(sensorNames == sensorName);
+            % Looping over different actions
             for k=0:numOfActions-1
                 rowNumber = numOfFeatures*k + featureIndex;
+                % Applying techniques
                 switch statisticalFeatures(j)
                     case "FFT"
                         contentAfterAlgo = abs(fft(content(rowNumber,1:end)));
                         contentAfterAlgo = sort(contentAfterAlgo,'descend');
-                        contentAfterAlgo = contentAfterAlgo(:,1:3);% 3 interesting points from fft
+                        contentAfterAlgo = contentAfterAlgo(:,1:numberOfPeakValues);% num of interesting points from fft
                     case "DWT"
                         contentIndividual = content(rowNumber,1:end);
                         nanIndex = find(isnan(contentIndividual));
                         contentIndividual(nanIndex) = 0;
                         contentAfterAlgo = dwt(contentIndividual,'sym4');
-                        % contentAfterAlgo = dwt(content(rowNumber,1:end),'sym4');
-                        contentAfterAlgo = contentAfterAlgo(:,1:3);% 3 interesting points from dwt
+                        contentAfterAlgo = sort(contentAfterAlgo,'descend');
+                        contentAfterAlgo = contentAfterAlgo(:,1:numberOfPeakValues);% num of interesting points from dwt
                     case "RMS"
                         contentAfterAlgo = rms(content(rowNumber,1:end));
                     case "STD"
                         contentAfterAlgo = std(content(rowNumber,1:end));
+                    case "AVG"
+                        contentAfterAlgo = std(content(rowNumber,1:end));
                     otherwise
                         contentAfterAlgo = rms(content(rowNumber,1:end));
                 end
+                % Concatenating different actions of the same gesture
                 if isempty(concatenatedContentAfterAlgo)
                     concatenatedContentAfterAlgo = contentAfterAlgo;
                 else
@@ -62,6 +73,7 @@ for i=1:length(words)
                     xAxisNames = cat(2,xAxisNames, strcat(algoName,'-',sensorName,'-',num2str(p)));
                 end
             end
+            % Stacking the features side by side
             if isempty(concatenatedContentAfterAllAlgo)
                 concatenatedContentAfterAllAlgo = concatenatedContentAfterAlgo;
             else
@@ -69,6 +81,7 @@ for i=1:length(words)
             end
         end
     end
+    % Writing the output to csc
     csvwrite(strcat(outputFolderName,'/',words(i),'.csv'),concatenatedContentAfterAllAlgo);
     [rows,col] = size(concatenatedContentAfterAllAlgo);
     for n=1:rows
