6a2b11dd8b165e686007f1dc4db5ba4f4ea9a22a
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2018-04-06 17:38:52 -0700

    Added report.csv

--- Assignment 3a/assignment_3_task_2_classify.m
+++ Assignment 3a/assignment_3_task_2_classify.m
@@ -7,6 +7,7 @@ classifiers = ["DT","SVM","NN"];
 % Looping over different groups as user dependent analysis
 userNameRegex = strcat(inputFolder,'/','DM*');
 userNames = dir(char(userNameRegex));
+results = [];
 for u=1:length(userNames)
     userName = userNames(u).name;
     trainFileName = strcat(inputFolder,'/',userName,'/','training_data.csv');
@@ -17,13 +18,13 @@ for u=1:length(userNames)
     testContent = table2array(testFile);
     [numOfTrainActions,~] = size(trainContent);
     [numOfTestActions,~] = size(testContent);
-    fprintf('Metrics of %s\n',userName);
+%     fprintf('Metrics of %s\n',userName);
     for i=1:length(words)
-        fprintf('\tGesture: %s\n',words(i));
+%         fprintf('\tGesture: %s\n',words(i));
         trainLabels = trainContent(1:numOfTrainActions,6) == i;
         testLabels = testContent(1:numOfTestActions,6) == i;
         for j=1:length(classifiers)
-            fprintf('\t\tMachine: %s\n',classifiers(j));
+%             fprintf('\t\tMachine: %s\n',classifiers(j));
             switch classifiers(j)
                 case "DT"
                     model = fitctree(trainContent(1:end,1:end-1),trainLabels);
@@ -53,10 +54,18 @@ for u=1:length(userNames)
             recall = TP / (TP + FN);
             F1 = (2 * precision * recall) / (precision + recall);
             accuracy = sum(predictedLabels == testLabels) / numOfTestActions;
-            fprintf('\t\t\tAccuracy: %f\n',accuracy);
-            fprintf('\t\t\tPrecision: %f\n',precision);
-            fprintf('\t\t\tRecall: %f\n',recall);
-            fprintf('\t\t\tF1 Score: %f\n',F1);
+%             fprintf('\t\t\tAccuracy: %f\n',accuracy);
+%             fprintf('\t\t\tPrecision: %f\n',precision);
+%             fprintf('\t\t\tRecall: %f\n',recall);
+%             fprintf('\t\t\tF1 Score: %f\n',F1);
+            result = [userName, words(i), classifiers(j), num2str(accuracy), num2str(precision), num2str(recall), num2str(F1)];
+            if isempty(results)
+                results = result;
+            else
+                results = cat(1, results, result);
+            end
         end
     end
 end
+T = table(results(:,1),results(:,2),results(:,3),results(:,4),results(:,5),results(:,6),results(:,7),'VariableNames',{'User' 'Gesture' 'Machine' 'Accuracy' 'Precision' 'Recall' 'F1'});
+writetable(T,'report.csv');
